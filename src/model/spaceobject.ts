0594f2918aef15cc3304bc7db45eb4bd76afcbb6
Justin Hugon <jhugon@users.noreply.github.com>
2022-08-30 09:57:54 -0500

    feat: added src/model/spaceobject.ts

---
+++ src/model/spaceobject.ts
@@ -0,0 +1,27 @@
+
+export interface Vector {
+    x: number;
+    y: number;
+}
+
+// All in SI base units
+export interface SpaceObject {
+    position: Vector;
+    velocity: Vector;
+    acceleration: Vector;
+    thrust: Vector; // actual acceleration due to thrust
+    thrustNominal: number; // nominal thrust value
+    mass: number;
+};
+
+export function init(mass: number, thrustNominal: number) {
+    const result: SpaceObject = {
+        position: {x: 0., y:0.};
+        velocity: {x: 0., y:0.};
+        acceleration: {x: 0., y:0.};
+        thrustRelative: {x: 0., y:0.};
+        thrustNominal: thrustNominal;
+        mass: mass;
+    };
+    return result
+}
