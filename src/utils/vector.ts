e8281a980e55d7356c46600cb19a2944987e5c7a
Justin Hugon <jhugon@users.noreply.github.com>
2022-08-30 21:51:46 -0500

    feat(vector): added modifier methods

--- src/utils/vector.ts
+++ src/utils/vector.ts
@@ -6,4 +6,33 @@ export class Vector {
         this.x = x;
         this.y = y;
     }
+
+    magnitude(): number {
+        return Math.sqrt(this.x*this.x+this.y*this.y);
+    }
+
+    add(other: Vector): void {
+        this.x += other.x;
+        this.y += other.y;
+    }
+
+    subtract(other: Vector): void {
+        this.x -= other.x;
+        this.y -= other.y;
+    }
+
+    multiply(sf: number): void {
+        this.x *= sf;
+        this.y *= sf;
+    }
+
+    divide(sf: number): void {
+        this.x /= sf;
+        this.y /= sf;
+    }
+
+    normalize(): void {
+        const mag = this.magnitude();
+        this.divide(mag);
+    }
 }
