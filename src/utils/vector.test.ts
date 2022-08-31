ed86d000231f419aa02f8e50b3879bf1fa26a1ce
Justin Hugon <jhugon@users.noreply.github.com>
2022-08-31 13:01:59 -0500

    refactor(vector): moved x1, x2, etc. out as globals

--- src/utils/vector.test.ts
+++ src/utils/vector.test.ts
@@ -2,37 +2,30 @@
 import {describe, expect, test} from '@jest/globals';
 import {Vector} from "./vector";
 
+const x1 = 4.;
+const y1 = -5;
+const x2 = 2.3;
+const y2 = 50.2;
+
 describe("Vector", () => {
   test("constructor", () => {
-      const x = 4.;
-      const y = -5.;
-      const vec = new Vector(x,y);
-      expect(vec.x).toBe(x);
-      expect(vec.y).toBe(y);
+      const vec = new Vector(x1,y1);
+      expect(vec.x).toBe(x1);
+      expect(vec.y).toBe(y1);
   });
   test("magnitude", () => {
-      const x = 4.;
-      const y = -5.;
-      const vec = new Vector(x,y);
-      expect(vec.magnitude).toBe(Math.sqrt(x*x+y*y));
+      const vec = new Vector(x1,y1);
+      expect(vec.magnitude).toBe(Math.sqrt(x1*x1+y1*y1));
   });
   test("angleRadians", () => {
-      const x = 4.;
-      const y = -5.;
-      const vec = new Vector(x,y);
-      expect(vec.angleRadians).toBe(Math.atan2(y,x));
+      const vec = new Vector(x1,y1);
+      expect(vec.angleRadians).toBe(Math.atan2(y1,x1));
   });
   test("angleDegrees", () => {
-      const x = 4.;
-      const y = -5.;
-      const vec = new Vector(x,y);
-      expect(vec.angleDegrees).toBe(Math.atan2(y,x)*180/Math.PI);
+      const vec = new Vector(x1,y1);
+      expect(vec.angleDegrees).toBe(Math.atan2(y1,x1)*180/Math.PI);
   });
   test("add", () => {
-      const x1 = 4;
-      const y1 = -5;
-      const x2 = 2.3;
-      const y2 = 50.2;
       const vec1 = new Vector(x1,y1);
       const vec2 = new Vector(x2,y2);
       vec1.add(vec2);
@@ -42,10 +35,6 @@ describe("Vector", () => {
       expect(vec2.y).toBe(y2);
   });
   test("static add", () => {
-      const x1 = 4;
-      const y1 = -5;
-      const x2 = 2.3;
-      const y2 = 50.2;
       const vec1 = new Vector(x1,y1);
       const vec2 = new Vector(x2,y2);
       const vec3 = Vector.add(vec1,vec2);
@@ -57,10 +46,6 @@ describe("Vector", () => {
       expect(vec3.y).toBe(y1+y2);
   });
   test("subtract", () => {
-      const x1 = 4;
-      const y1 = -5;
-      const x2 = 2.3;
-      const y2 = 50.2;
       const vec1 = new Vector(x1,y1);
       const vec2 = new Vector(x2,y2);
       vec1.subtract(vec2);
@@ -70,10 +55,6 @@ describe("Vector", () => {
       expect(vec2.y).toBe(y2);
   });
   test("static subtract", () => {
-      const x1 = 4;
-      const y1 = -5;
-      const x2 = 2.3;
-      const y2 = 50.2;
       const vec1 = new Vector(x1,y1);
       const vec2 = new Vector(x2,y2);
       const vec3 = Vector.subtract(vec1,vec2);
