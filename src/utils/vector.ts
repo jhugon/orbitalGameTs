5a40b8e737d85053844aa59b19980c22a94a52ff
Justin Hugon <jhugon@users.noreply.github.com>
2022-08-30 21:56:01 -0500

    feat(vector): add static methods for operations

--- src/utils/vector.ts
+++ src/utils/vector.ts
@@ -16,9 +16,21 @@ export class Vector {
         this.y += other.y;
     }
 
+    static add(a: Vector, b: Vector): Vector {
+        const result = new Vector(a.x,a.y);
+        result.add(b);
+        return result;
+    }
+
     subtract(other: Vector): void {
         this.x -= other.x;
         this.y -= other.y;
+    }
+
+    static subtract(a: Vector, b: Vector): Vector {
+        const result = new Vector(a.x,a.y);
+        result.subtract(b);
+        return result;
     }
 
     multiply(sf: number): void {
@@ -26,13 +38,32 @@ export class Vector {
         this.y *= sf;
     }
 
+    static multiply(a: Vector, sf: number): Vector {
+        const result = new Vector(a.x,a.y);
+        result.multiply(sf);
+        return result;
+    }
+
     divide(sf: number): void {
         this.x /= sf;
         this.y /= sf;
+    }
+
+    static divide(a: Vector, sf: number): Vector {
+        const result = new Vector(a.x,a.y);
+        result.divide(sf);
+        return result;
     }
 
     normalize(): void {
         const mag = this.magnitude();
         this.divide(mag);
     }
+
+    static normalize(a: Vector): Vector {
+        const result = new Vector(a.x,a.y);
+        result.normalize();
+        return result;
+    }
+
 }
